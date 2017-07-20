bcfe5149cbb85134d7cbef1021d467215797daf2
Evgeny Mikhaylov <fn-mea@yandex.ru>
2017-07-20 15:05:42 +0300

    first correct version

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,26 @@
-webpack = require('webpack');
-path = require('path');
+var webpack = require('webpack');
+var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 webpackConfig = {
     context: __dirname,
     entry: {
-<<<<<<< Updated upstream
-        bundle: './app.js'
-=======
         bundle: './app/entry.js'
->>>>>>> Stashed changes
     },
-    target: "node",
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, "/build"),
+        path: path.join(__dirname, "/static"),
         library: '[name]'
     },
     resolve: {
-        extensions: ['.html', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     },
     devtool: '#cheap-module-source-map',
     module: {
         loaders: [
             {
                 test: /\.(js|jsx)$/,
-                exclude: [/node_modules/],
                 loader: "babel-loader",
                 query: {
                     presets: ['react', 'es2015', 'stage-0', 'stage-1', 'stage-2', 'stage-3']
@@ -37,8 +31,6 @@ webpackConfig = {
                 loader: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: 'css-loader!resolve-url-loader!sass-loader?sourceMap'
-                    /*use: 'css-loader!sass-loader?sourceMap'*/
-                    /*use: 'css-loader!resolve-url!sass-loader?sourceMap'*/
                 })
             },
             {
